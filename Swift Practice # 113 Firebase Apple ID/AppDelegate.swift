ee7399d1c085adb6ea3487ab82b194856ed04823
Dogpa <dogpa0130@gmail.com>
2021-11-13 01:31:59 +0800

    demo

--- Swift Practice # 113 Firebase Apple ID/AppDelegate.swift
+++ Swift Practice # 113 Firebase Apple ID/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        if let user = Auth.auth().currentUser {
+            print("You are sign in as \(user.uid) email:\(user.email)")
+        }
         return true
     }
 
